8fb85235180daf27df5a643a8d0f93908b3cd481
tarasyakymiuk <taras.yakymiuk@rolique.io>
2022-04-17 19:07:57 +0300

    cleanup

--- dist/cjs/hooks/useLogging.d.ts
+++
@@ -1,7 +0,0 @@
-import Logging, { ILoggingOptions } from '../library/Logging';
-/**
- * A hook for a simple logging library.
- * @param options  Options to pass our logger.
- * @returns object of type Logging
- */
-export declare const useLogging: (options?: ILoggingOptions | undefined) => [Logging];
